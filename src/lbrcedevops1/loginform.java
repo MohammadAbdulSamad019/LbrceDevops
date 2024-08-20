e910073d4bbbc320bc3a87a8ff1e81b3d5121b66
AdmiN <AdmiN@ITWS070>
2024-08-20 11:08:06 +0530

    commit

--- src/lbrcedevops1/loginform.java
+++ src/lbrcedevops1/loginform.java
@@ -4,23 +4,9 @@ public class Welcome
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Please enter your name: ");
-        
-        // Read the user's input
-
         String userName = scanner.nextLine();
-
-        // Display a personalized welcome message
-
         System.out.println("Welcome to the app, " + userName + "!");
-
-
-
-        // Close the Scanner object to prevent resource leaks
-
         scanner.close();
-
     }
-
 }
