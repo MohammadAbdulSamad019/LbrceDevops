f2ca8f818a013f148c5a5453506316bfa3b3342e
AdmiN <AdmiN@ITWS070>
2024-08-20 11:30:57 +0530

    commit

--- src/lbrcedevops1/loginform.java
+++ src/lbrcedevops1/loginform.java
@@ -1,12 +1,20 @@
+package lbrcedevops1;
 import java.util.Scanner;
-public class Welcome 
+public class loginform
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
         Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
         System.out.print("Please enter your name: ");
+        // Read the input from the user
         String userName = scanner.nextLine();
+
+        // Print a welcome message including the user's name
         System.out.println("Welcome to the app, " + userName + "!");
+
+        // Close the scanner to free up resources
         scanner.close();
     }
 }
