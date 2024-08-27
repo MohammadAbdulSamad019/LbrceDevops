5e247023072167e1df817e59ef3a2d95bd17cf4d
AdmiN <AdmiN@ITWS0041>
2024-08-27 10:47:14 +0530

    thank you

--- src/H1/pullrequest.java
+++ src/H1/pullrequest.java
@@ -1,10 +1,10 @@
-package lbrcedevops1;
+package H1;
 
-public class welcome {
+public class pullrequest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello welcome");
-		
+
 	}
+
 }
